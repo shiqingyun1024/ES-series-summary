e65e59ac0be8fa382bd27481b809cb868b7b18e2
814657780 <18037818812@163.com>
2024-07-11 09:43:09 +0800

    大文件上传-利用时间切片来计算md5值

---
+++ 大文件上传/2、calculateHash.js
@@ -0,0 +1,39 @@
+// 这是花果山大圣写的一篇文章里面的大文件上传改造里面的东西
+
+async calculateHashIdle(chunks){
+    return new Promise(resolve => {
+        const spark = new SparkMD5.ArrayBuffer();
+        let count = 0;
+        // 根据文件内容追加计算
+        const appendToSpark = async file => {
+            return new Promise(resolve => {
+                const reader = new FileReader();
+                reader.readAsArrayBuffer(file);
+                reader.onload = e => {
+                    spark.append(e.target.result);
+                    resolve();
+                }
+            })
+        };
+        const workLoop = async deadline => {
+            // 有任务，并且当前帧还没结束
+            while (count < chunks.length && deadline.timeRemaining() > 1) {
+                await appendToSpark(chunks[count].file);
+                count++;
+                // 没有了 计算完毕
+                if (count < chunks.length) {
+                    // 计算中
+                    this.hashProgress = Number(
+                        ((100 * count) / chunks.length).toFixed(2)
+                    )
+                } else {
+                    // 计算完毕
+                    this.hashProgress = 100;
+                    resolve(spark.end());
+                }
+            }
+            window.requestIdleCallback(workLoop);
+        };
+        window.requestIdleCallback(workLoop);
+    })
+}
